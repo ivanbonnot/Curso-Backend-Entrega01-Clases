678f0353bbea6c424c4299b1842016c68d4e4207
Ivan Bonnot <ivanbonnot@gmail.com>
2022-11-24 21:17:47 -0300

    listo

--- desafioDos/app.js
+++ desafioDos/app.js
@@ -2,106 +2,120 @@ const fs = require('fs')
 
 
 class Contenedor {
-    constructor(file) {
-        this.file = file
+  constructor(file) {
+    this.file = file
+  }
+
+  async save(product) {
+    const productos = await this.getAll()
+    try {
+      let idGen
+      productos.length === 0
+        ? idGen = 1
+        : idGen = productos[productos.length - 1].id + 1
+
+      const prodNuevo = { id: idGen, ...product }
+      productos.push(prodNuevo)
+      await this.saveFile(this.file, productos)
+      return idGen
+
+    } catch (err) {
+      console.log(`Error: ${err}`)
     }
+  }
 
-    async save(file, productos) {
-        try {
-            await fs.promises.writeFile(file, productos)
-        } catch (err) {
-            console.log(`Error: ${err}`)
-        }
-
+  async saveFile(file, productos) {
+    try {
+      await fs.promises.writeFile(
+        file, JSON.stringify(productos, null, 2)
+      )
+    } catch (err) {
+      console.log(`Error: ${err}`)
     }
+  }
 
-    async getById(id) {
+  async getById(id) {
+    const productos = await this.getAll()
+    try {
+      const prod = productos.find(item => item.id === id)
+      return prod ? prod : null
 
+    } catch (err) {
+      console.log(`Error: ${err}`)
     }
+  }
 
-    async getAll() {
-        try {
-            const productos = await fs.promises.readFile(this.file, 'utf-8')
-            return JSON.parse(productos)
+  async getAll() {
+    try {
+      const productos = await fs.promises.readFile(this.file, 'utf-8')
+      return JSON.parse(productos)
 
-        } catch (err) {
-            console.log(`Error: ${err}`)
-        }
+    } catch (err) {
+      console.log(`Error: ${err}`)
     }
+  }
 
-    async deleteById(id) {
+  async deleteById(id) {
+    let productos = await this.getAll()
 
+    try {
+      productos = productos.filter(item => item.id != id)
+      await this.saveFile(this.file, productos)
+
+    } catch (err) {
+      console.log(`Error: ${err}`)
     }
+  }
 
-    async deleteAll() {
-
-    }
+  async deleteAll() {
+    await this.saveFile(this.file, [])
+  }
 
 }
 
 
+const productos = new Contenedor('productos.txt')
+
+const test = async () => {
+  try {
+
+    let array = await productos.getAll()
+    console.log("-----Traer todos los productos-----")
+    console.log(array)
+
+    await productos.save(
+      {
+        "image": "https://",
+        "title": "Impresora 3D Nueva Generacion",
+        "price": 1200,
+        "description": "bla bla bla"
+        
+      }
+    )
+    array = await productos.getAll()
+    console.log("-----Traer todos los productos junto con el agregado nuevo-----")
+    console.log(array)
+
+    let idProd = await productos.getById(2)
+    console.log("-----Traer el producto con id = 2-----")
+    console.log(idProd)
+    
+
+    await productos.deleteById(3)
+    array = await productos.getAll()
+    console.log("-----Borrado el produto con id = 3-----")
+    console.log(array)
+
+    await productos.deleteAll()
+    array = await productos.getAll()
+    console.log("-----Borrado todos los productos-----")
+    console.log(array)
 
 
-
-
-
-//Selectores
-const nombre = document.getElementById('nombre')
-const apellido = document.getElementById("apellido")
-const nombreMascota = document.getElementById("nombreMascota")
-const nombreLibro = document.getElementById("nombreLibro")
-const autorLibro = document.getElementById("autorLibro")
-const enviar = document.getElementById('enviar')
-const reiniciar = document.getElementById('reinicio')
-const resultado = document.getElementById('resultado')
-
-
-reiniciar.disabled = true
-
-
-enviar.addEventListener('click', () => validarInput())
-reiniciar.addEventListener('click', () => LimpiarHTML())
-
-
-//Validar input
-function validarInput() {
-    if (nombre.value === '' || apellido.value === '' || nombreMascota.value === '' || nombreLibro.value === '' || autorLibro.value === '') {
-        console.log("ingrese todos los datos")
-    } else {
-        agregar()
-    }
+  } catch (err) {
+    console.log(err)
+  }
 }
 
-let usuario2 = new Usuario()
+test()
 
-function agregar() {
-    usuario2 = new Usuario(nombre.value, apellido.value)
-    usuario2.addMascota(`${nombreMascota.value}`)
-    usuario2.addBook(`${autorLibro.value}`, `${nombreLibro.value}`)
-    mostrarHTML()
-}
-
-function mostrarHTML() {
-    console.log(usuario2.getFullName())
-    const parrafo = document.createElement('div');
-    parrafo.innerHTML = `<p>Nombre y apellido: ${usuario2.getFullName()}</p> 
-                         <p>Cantidad de mascotas: ${usuario2.countMascotas()} </p>
-                         <p>Nombre de los libros: ${usuario2.getBooksNames()} </p>`
-    resultado.appendChild(parrafo);
-    enviar.disabled = true
-    reiniciar.disabled = false
-}
-
-//Limpiar formulario
-function LimpiarHTML() {
-    while (resultado.firstChild) {
-        resultado.removeChild(resultado.firstChild);
-    }
-    reiniciar.disabled = true
-    enviar.disabled = false
-    nombre.value = ''
-    apellido.value = ''
-    nombreMascota.value = ''
-    nombreLibro.value = ''
-    autorLibro.value = ''
-}
